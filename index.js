e20eb9ae286db9718c023861af812ebabdb07394
ErTavo <66528756+ErTavo@users.noreply.github.com>
2024-12-05 15:20:36 -0600

    correciones cors

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ const userRoutes = require('./routes/users');
 
 const PORT = process.env.PORT || 3000;
 
+const allowedOrigins = [
+  'http://localhost:3000', 
+  'https://tu-frontend.vercel.app',
+  '*' 
+];
 
-app.use(cors('*'));
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin) || allowedOrigins.includes('*')) {
+      callback(null, true); 
+    } else {
+      callback(new Error('Origen no permitido por CORS')); 
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 
 
 app.use(express.json());
 
-app.use('/api', userRoutes);
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('¡Servidor funcionando!');
